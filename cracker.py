45d93e3e32cc7ddf820404c5274f929f6cd92352
ayazhan <karaayaz_@outlook.com>
2014-07-24 01:54:20 +0300

    Create cracker.py

---
+++ cracker.py
@@ -0,0 +1,24 @@
+# -*- coding: cp1254 -*-
+print """ ##### IDMCracker V1 #####
+Coder: Kara Ayaz	e-mail: karaayaz_@outlook.com
+Lütfen bilgileri eksiksiz giriniz...
+"""
+
+eposta = raw_input("e-posta adresinizi giriniz: ")
+print ""
+print "IDM internet sitesi ile bağlantı kesiliyor"
+print ""
+print "Kayıt defterine gerekli bilgiler ekleniyor"
+
+
+def yaz():
+    print "merhaba dünya"
+    #dosya = open("C:\\WINDOWS\\system32\\drivers\\etc\\hosts", "a")
+    #dosya.write("""
+#127.0.0.1 www.internetdownloadmanager.com
+#0.0.0.0 www.internetdownloadmanager.com
+#207.44.199.159 registeridm.com
+#207.44.199.16 registeridm.com
+#""")
+    #dosya.close()
+yaz()
